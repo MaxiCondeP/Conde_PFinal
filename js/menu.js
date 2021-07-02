6d9801bab0bda87d5dd48274078afba1c5e6b308
Maxi Conde <maxicondep@gmail.com>
2021-07-01 23:34:14 -0300

    Cambios previos a entrega final1

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+$(document).ready(main);
+
+var contador = 1;
+
+function main(){
+	$('.logoMenu').click(function(){
+		// $('menuItem').toggle(); 
+
+        if(contador == 1){
+			$('.miMenu').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('.miMenu').animate({
+				left: '-100%'
+			});
+		}
+
+	});
+};
